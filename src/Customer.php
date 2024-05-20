049ef02bc1da8b8023792320697a5ff21046b204
salwashs <salwa2105salsabila@gmail.com>
2024-05-20 18:03:01 +0800

    first commit

---
+++ src/Customer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Salwa\Belajar;
+
+class Customer
+{
+    public function __construct(private string $name)
+    {
+    }
+
+    public function sayHello(string $name = "Guest"): string
+    {
+        return "Hello " . $name . ", my name is " . $this->name;
+    }
+}
